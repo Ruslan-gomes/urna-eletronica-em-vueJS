e63311f0927461423f957dc3347194ee1e0b9c1a
filipe_smith <ruslanfilipe123@gmail.com>
2021-01-19 16:52:33 -0300

    implementação da urna

---
+++ src/components/teclado.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="urna-teclado">
+        <div class="urna-teclado-numerico">
+            <div class="urna-teclado-numerico-linhas">
+                <button @click="adicionarNumero(1)">1</button>
+                <button @click="adicionarNumero(2)">2</button>
+                <button @click="adicionarNumero(3)">3</button>
+            </div>
+            <div class="urna-teclado-numerico-linhas">
+                <button @click="adicionarNumero(4)">4</button>
+                <button @click="adicionarNumero(5)">5</button>
+                <button @click="adicionarNumero(6)">6</button>
+            </div>
+            <div class="urna-teclado-numerico-linhas">
+                <button @click="adicionarNumero(7)">7</button>
+                <button @click="adicionarNumero(8)">8</button>
+                <button @click="adicionarNumero(9)">9</button>
+            </div>
+            <div class="urna-teclado-numerico-linhas">
+                <button @click="adicionarNumero(0)">0</button>
+            </div>
+        </div>
+
+        <div class="urna-teclado-acoes">
+            <button @click="votoBranco" class="btn-branco">BRANCO</button>
+            <button @click="corrigir" class="btn-corrige">CORRIGE</button>
+            <button @click="confirmar" class="btn-confirma">CONFIRMA</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'teclado',
+    props: {
+        adicionarNumero: Function,
+        corrigir: Function,
+        confirmar: Function,
+        votoBranco: Function
+    }
+}
+</script>
+
+
+<style scoped>
+
+.urna-teclado{
+    width: 40%;
+    height: 100%;
+    background-color: var(--ballot-box-keyboard-color);
+    border-radius: 5px;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+.urna-teclado-numerico{
+    widows: 100%;
+}
+
+.urna-teclado-numerico-linhas{
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 20px;
+}
+
+.urna-teclado-numerico button{
+    background-color: var(--ballot-box-keyboard-button-color);
+    color: var(--light-text-color);
+    font-size: 30px;
+    border-radius: 5px;
+    width: 80px;
+    height: 50px;
+}
+
+.urna-teclado-acoes{
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+}
+
+.urna-teclado-acoes button{
+    color: var(--dark-text-color);
+    font-size: 15px;
+    border-radius: 5px;
+    width: 30%;
+    height: 50px;
+}
+
+.btn-branco{
+    background-color: var(--ballot-box-white-button-color);
+}
+
+.btn-corrige{
+    background-color: var(--ballot-box-correct-button-color);
+}
+
+.btn-confirma{
+    background-color: var(--ballot-box-confirm-button-color);
+}
+</style>
---
+++ src/components/tela.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="urna-tela">
+
+        <div v-if="tela != 'fim'" class="urna-tela-voto">
+            <div class="urna-tela-voto-textos">
+                <div class="urna-tela-voto-textos-titulo">Seu voto para:</div>
+                <div class="urna-tela-voto-tipo">{{tela}}</div>
+            </div>
+
+            <div class="urna-tela-voto-numeros">
+                Números:
+                <div class="urna-tela-voto-numero" v-for="(value, key) in numeroVoto.padEnd(quantidadeNumeros, ' ')" :key="key">
+                    {{value}}
+                </div>                
+            </div>
+
+            <div class="urna-tela-voto-descricoes">
+                <div class="urna-tela-voto-descricao">
+                Nome:  {{candidato.nome ? candidato.nome : '______________ '}}
+            </div>
+
+            <div class="urna-tela-voto-descricao">
+                 Partido:  {{candidato.partido ? candidato.partido : '______________ '}}
+            </div>
+            </div>
+                <div v-if="candidato.imagem" class="urna-tela-voto-imagem">
+                <img :src="candidato.imagem" alt="imagem do candidato">
+            </div>
+        </div>
+        <div v-if="tela == 'fim'" class="urna-tela-fim">
+            FIM
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'tela',
+    props: {
+        tela: String,
+        numeroVoto: String,
+        quantidadeNumeros: Number,
+        candidato: Object
+    }
+}
+</script>
+
+<style scoped>
+.urna-tela{
+    width: 55%;   
+    height: 100%;
+    background-color: var(--ballot-box-screen-color);
+    border-radius: 5px;
+    border-color: 2px solid var(--light-border-color);
+    padding: 20px;
+    color: var(--dark-text-color);
+}
+
+.urna-tela-voto{
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+}
+
+.urna-tela-voto-textos{
+    flex: 1;
+}
+
+.urna-tela-voto-tipo{
+    font-size: 50px;
+    margin: 10px 20px;
+}
+
+.urna-tela-voto-numeros{
+    display: flex;
+    align-items: center;
+}
+
+.urna-tela-voto-numero{
+    width: 45px;
+    height: 55px;
+    border: 1px solid var(--dark-border-color);
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 30px;
+}
+
+.urna-tela-voto-descricoes{
+    display: flex;
+    flex-direction: column;
+}
+
+.urna-tela-voto-descricao{
+    margin-top: 20px;
+}
+
+.urna-tela-voto-imagem img{
+    width: 110px;
+    height: 150px;
+    border: 1px solid var(--dark-border-color);
+    float: right;
+}
+
+.urna-tela-fim{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 120px;
+    width: 100%;
+    height: 100%;
+}
+</style>
